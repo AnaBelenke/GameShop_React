80ea7fbbdda9597380d88bfd772280eebdc53007
AnaBelenke <ana.cbs008@gmail.com>
2024-07-04 15:00:12 -0300

    Commit menu

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.css';
+import SideMenu from '../components/SideMenu';
+
+function Main() {
+    return (
+        <main>
+            <SideMenu> </SideMenu>
+        </main>
+    );
+}
+
+export default Main;
