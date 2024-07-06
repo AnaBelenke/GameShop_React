43fa0086655cf9e526a3f2503bd8f373fecf7ab8
AnaBelenke <ana.cbs008@gmail.com>
2024-07-06 14:50:11 -0300

    Commit Header

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.css';
 import SideMenu from '../components/SideMenu';
+import Header from './Header';
 
 function Main() {
+    const [active, setActive] = useState(false);
+
+    const handelToggleActive = () => {
+        setActive (!active);
+    }
+
     return (
         <main>
-            <SideMenu> </SideMenu>
+            <SideMenu active={active} />
+            <div className={`banner ${active ? 'active' : undefined}`}>
+                <Header toggleActive={handelToggleActive} />
+            </div>
         </main>
     );
 }
