03b322fe714a408486d39589a5db4ac169b52770
AnaBelenke <ana.cbs008@gmail.com>
2024-07-10 15:07:24 -0300

    Commit Carousel Slide

--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import './gameSwiper.css'
 
-function GameSwiper() {
-  return (
-    <div>
-      
-    </div>
-  )
+import { EffectCoverflow, Navigation, Autoplay } from 'swiper/modules';
+import GameSlide from './GameSlide';
+
+function GameSwiper({ games }) {
+    const [active, setActive] = useState(false)
+
+    const handleToggleVideo = () => {
+        setActive(!active)
+    }
+
+    return (
+        <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            navigation={true}
+            loop={true}
+            centeredSliders={true}
+            slidesPerView={'auto'}
+            coverflowEffect={{
+                rotate: 35,
+                stretch: 200,
+                depth: 250,
+                modifier: 1,
+                slideShadowns: true,
+            }}
+            // Autoplay={{
+            //     delay: 2500, 
+            //     disableOnInteraction: false,
+            // }}
+            modules={[EffectCoverflow, Navigation, Autoplay]}
+            className="gameSwiper"
+        >
+            {games.map(game => (
+                <SwiperSlide key={game._id}>
+                    <GameSlide
+                        game={game}
+                        active={active}
+                        toggleVideo={handleToggleVideo}
+                    />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    )
 }
 
 export default GameSwiper
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,20 @@ import GameSwiper from '../components/GameSwiper';
 
 function Home({ games }) {
     return (
-        <section id='home' className="home">
+        <section id='home' className="home active">
             <div className="conteiner-fluid">
                 <div className="row">
                     <GameSwiper games={games} />
+                </div>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h2 className="sectionTittle">
+                            Games on promotion
+                        </h2>
+                    </div>
+                    <div className="col-lg-6">
+                        
+                        </div>
                 </div>
             </div>
         </section>
