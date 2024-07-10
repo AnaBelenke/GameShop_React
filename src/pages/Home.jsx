31c59c8e711a5207fbf705184ef1679724402561
AnaBelenke <ana.cbs008@gmail.com>
2024-07-10 16:07:35 -0300

    Commit item card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.css';
 import GameSwiper from '../components/GameSwiper';
+import GameCard from '../components/GameCard';
 
 function Home({ games }) {
     return (
@@ -11,13 +12,18 @@ function Home({ games }) {
                 </div>
                 <div className="row">
                     <div className="col-lg-6">
-                        <h2 className="sectionTittle">
+                        <h2 className="sectionTitle">
                             Games on promotion
                         </h2>
                     </div>
-                    <div className="col-lg-6">
-                        
-                        </div>
+                    <div className="col-lg-6 d-flex justify-content-end align-items-center">
+                       <a href="#" className="viewMore">View More Games <i className="bi bi-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div className="row">
+                    {games.slice(0, 4).map(game => (
+                        <GameCard key={game._id} game={game} />
+                    ))}
                 </div>
             </div>
         </section>
